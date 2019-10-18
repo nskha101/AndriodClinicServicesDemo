c44ddcc630f730f43a2cac56cc39b8f33bf41443
Alex Degrace <adegr091@uottawa.ca>
2019-10-18 00:23:44 -0400

    Changed SignIn

--- app/src/main/java/com/example/seg2105/SignIn.java
+++ app/src/main/java/com/example/seg2105/SignIn.java
@@ -1,17 +1,24 @@
 package com.example.seg2105;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.os.Bundle;
 
 public class SignIn extends AppCompatActivity {
 
     //Make sure that the database reference is our database
-    public FirebaseDatabase database = FirebaseDatabase.getInstance();
-    final DatabaseReference UserRef = database.getReference("users");
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference userRef = database.getReference("users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,9 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
     }
 
+    public boolean infoChecker(final String currentUsername, final String currentPassword){
 
+
+
+    }
 }
