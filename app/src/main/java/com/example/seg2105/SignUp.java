95e118657833df85df0db0cda9293ff4aa14fe23
nskha101 <nskha101@uottawa.ca>
2019-10-17 23:20:14 -0400

    gradleupdate2

---
+++ app/src/main/java/com/example/seg2105/SignUp.java
@@ -0,0 +1,29 @@
+package com.example.seg2105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void onCreateClick(View view){
+        //feilds to make: String username,String email, String password, String name, String familyName
+        EditText username = findViewById(R.id.usernamefeild);
+        EditText username = findViewById(R.id.usernamefeild);
+        EditText username = findViewById(R.id.usernamefeild);
+        EditText username = findViewById(R.id.usernamefeild);
+        EditText username = findViewById(R.id.usernamefeild);
+
+    }
+
+}
