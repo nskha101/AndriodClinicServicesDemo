9498e4a074d8a3c93cc1dfd46721585b6eb72819
nskha101 <nskha101@uottawa.ca>
2019-10-17 23:41:17 -0400

    signup page 1

--- app/src/main/java/com/example/seg2105/SignUp.java
+++ app/src/main/java/com/example/seg2105/SignUp.java
@@ -18,11 +18,19 @@ public class SignUp extends AppCompatActivity {
 
     public void onCreateClick(View view){
         //feilds to make: String username,String email, String password, String name, String familyName
-        EditText username = findViewById(R.id.usernamefeild);
-        EditText username = findViewById(R.id.usernamefeild);
-        EditText username = findViewById(R.id.usernamefeild);
-        EditText username = findViewById(R.id.usernamefeild);
-        EditText username = findViewById(R.id.usernamefeild);
+        EditText usernametextfeild = findViewById(R.id.usernamefeild);
+        EditText emailtextfeild = findViewById(R.id.emailfeild);
+        EditText passwordtextfeild = findViewById(R.id.passwordfeild); //convert to sha256
+        EditText nametextfeild = findViewById(R.id.namefeild);
+        EditText familyNametextfeild = findViewById(R.id.familyNamefeild);
+        //add radiobutton functionality for patient/employee pick (if patient radiobutton is picked, make role string = Patient, etc
+
+       String username = usernametextfeild.getText().toString();
+       String email = emailtextfeild.getText().toString();
+       String password = passwordtextfeild.getText().toString();
+       String name = nametextfeild.getText().toString();
+       String familyName = familyNametextfeild.getText().toString();
+       String role = roleText;
 
     }
 
