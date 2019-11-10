d9141bae5e08ae3c7e64bee1160e86e2812418b3
Lincoln <llayl068@uottawa.ca>
2019-11-10 17:22:53 -0500

    commit serviceadd5

--- app/src/main/java/com/example/seg2105/WelcomeScreen.java
+++ app/src/main/java/com/example/seg2105/WelcomeScreen.java
@@ -3,6 +3,7 @@ package com.example.seg2105;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -13,6 +14,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -24,27 +27,61 @@ import java.util.List;
 
 public class WelcomeScreen extends addService {
 
-    ListView listview;
-    List list = new ArrayList();
-    ArrayAdapter adapter;
-    Service[] servicelist = new Service[10];
-    int tail= 0;
+    private DatabaseReference mDatabase;
+
+    private ListView listView;
+
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
         TextView textView = (TextView) findViewById(R.id.UserName);
         textView.setText(MainActivity.getUser().getName());
         TextView text2 = (TextView) findViewById(R.id.role);
         text2.setText(MainActivity.getUser().getRole());
 
-       listview = (ListView)findViewById(R.id.list_view);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        /*add services*/
+       adapter = new ArrayAdapter<String> (this, android.R.layout.simple_expandable_list_item_1, arrayList);
 
-        adapter = new ArrayAdapter(WelcomeScreen.this,android.R.layout.simple_expandable_list_item_1,list);
-        listview.setAdapter(adapter);
+       listView = (ListView) findViewById(R.id.list_view);
+
+   /* mDatabase.addChildEventListener(new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            String string = dataSnapshot.getValue(String.class);
+            arrayList.add(string);
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+
+    */
     }
+
 
     public void onClick(View view){
         Intent intent = new Intent(getApplicationContext(), AdminScreen.class);
--- app/src/main/java/com/example/seg2105/addService.java
+++ app/src/main/java/com/example/seg2105/addService.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +27,8 @@ public class addService extends AppCompatActivity {
 
         String servicename = serviceNameInput.getText().toString();
         String rate = serviceRateInput.getText().toString();
+
+        Button button = (Button) findViewById(R.id.button);
 
     }
 
