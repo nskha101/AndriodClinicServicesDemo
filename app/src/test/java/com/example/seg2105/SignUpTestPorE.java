4d2c74fc6fdcf1e3eecbdaf09c5307fa341b4142
dstew056 <55411928+dstew056@users.noreply.github.com>
2019-11-10 23:42:23 -0500

    remove admin button

--- app/src/test/java/com/example/seg2105/SignUpTestPorE.java
+++ app/src/test/java/com/example/seg2105/SignUpTestPorE.java
@@ -8,5 +8,14 @@ public class SignUpTestPorE {
 
     @Test
     public void validatepatientorEmployee() {
+        String valid = "patient";
+        String valid2 = "employee";
+        String invalid = "notpatientoremployee";
+        SignUp su = new SignUp();
+
+        assertTrue(su.validatepatientorEmployee()(valid));
+        assertTrue(su.validatepatientorEmployee()(valid2));
+        assertFalse(su.validatepatientorEmployee(invalid));
+
     }
 }
