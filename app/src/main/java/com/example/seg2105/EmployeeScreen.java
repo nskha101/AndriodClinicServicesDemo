ab7fa36113f8642a8036c0dec8caaec6d0d297ad
Bensmrz <55411938+Bensmrz@users.noreply.github.com>
2019-11-18 18:23:27 -0500

    EmployeeScreen UI done

---
+++ app/src/main/java/com/example/seg2105/EmployeeScreen.java
@@ -0,0 +1,34 @@
+package com.example.seg2105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EmployeeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_screen);
+    }
+    public void goToDeleteEmployeeService(View view){
+        Intent intent = new Intent(getApplicationContext(), DeleteEmployeeService.class);
+        startActivity(intent);
+    }
+    public void goToAddEmployeeService(View view){
+        Intent intent = new Intent(getApplicationContext(), AddEmployeeService.class);
+        startActivity(intent);
+    }
+
+    public void goToClinicHours(View view){
+        Intent intent = new Intent(getApplicationContext(), ClinicHours.class);
+        startActivity(intent);
+    }
+
+    public void goToEmployeeHours(View view){
+        Intent intent = new Intent(getApplicationContext(), EmployeeHours.class);
+        startActivity(intent);
+    }
+}
