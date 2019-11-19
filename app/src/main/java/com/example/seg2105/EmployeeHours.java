d899b2389479e06800e35da3cc751ff81b65ee71
Bensmrz <55411938+Bensmrz@users.noreply.github.com>
2019-11-19 14:16:56 -0500

    EmployeeHours added

---
+++ app/src/main/java/com/example/seg2105/EmployeeHours.java
@@ -0,0 +1,20 @@
+package com.example.seg2105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EmployeeHours extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_hours);
+    }
+    public void goToEmployeeScreen(View view){
+        Intent intent = new Intent(getApplicationContext(), EmployeeScreen.class);
+        startActivity(intent);
+    }
+}
