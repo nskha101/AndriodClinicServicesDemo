45836ab418e9faa6e4b50d4b1b2dff6c984c4d38
AlexDegrace <alexdegrace345@gmail.com>
2019-11-20 17:05:41 -0500

    Work on ClinicWorkingHours to add dropdown menu for the day and hours.

--- app/src/main/java/com/example/seg2105/ClinicWorkingHours.java
+++ app/src/main/java/com/example/seg2105/ClinicWorkingHours.java
@@ -3,16 +3,19 @@ package com.example.seg2105;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class ClinicWorkingHours extends AppCompatActivity {
+public class ClinicWorkingHours extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public FirebaseDatabase database = FirebaseDatabase.getInstance();
     final DatabaseReference userRef = database.getReference("users");
@@ -21,45 +24,50 @@ public class ClinicWorkingHours extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinic_working_hours);
+        
+        Spinner daySpinner = findViewById(R.id.daySpinner);
+        Spinner startTimeSpinner = findViewById(R.id.startSpinner);
+        Spinner endTimeSpinner = findViewById(R.id.endTimeSpinner);
+
+        ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter.createFromResource(this, R.array.dayOfTheWeek, android.R.layout.simple_spinner_item);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+        daySpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> startAdapter = ArrayAdapter.createFromResource(this, R.array.timeSlot, android.R.layout.simple_spinner_item);
+        startAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        startTimeSpinner.setAdapter(startAdapter);
+        startTimeSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> endAdapter = ArrayAdapter.createFromResource(this, R.array.timeSlot, android.R.layout.simple_spinner_item);
+        endAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        endTimeSpinner.setAdapter(endAdapter);
+        endTimeSpinner.setOnItemSelectedListener(this);
     }
 
     public void onClick(View view){
-        RadioButton[] list = new RadioButton[7];
 
-        RadioButton monday = (RadioButton) findViewById(R.id.radioButton2);
-        RadioButton tuesday = (RadioButton) findViewById(R.id.radioButton3);
-        RadioButton wednesday = (RadioButton) findViewById(R.id.radioButton4);
-        RadioButton thursday = (RadioButton) findViewById(R.id.radioButton5);
-        RadioButton friday = (RadioButton) findViewById(R.id.radioButton6);
-        RadioButton saturday = (RadioButton) findViewById(R.id.radioButton7);
-        RadioButton sunday = (RadioButton) findViewById(R.id.radioButton8);
 
-        EditText starttime = (EditText) findViewById(R.id.starttime);
-        EditText endtime = (EditText) findViewById(R.id.starttime);
+        TextView textview = (TextView) findViewById(R.id.messageTextView);
 
-        list[0]=monday;
-        list[1]=tuesday;
-        list[2]=wednesday;
-        list[3]=thursday;
-        list[4]=friday;
-        list[5]=saturday;
-        list[6]=sunday;
-
-        TextView textview = (TextView) findViewById(R.id.textView44);
-
-       for (int i=0; i<7; i++){
+       /*for (int i=0; i<7; i++){
             boolean checked = (list[i]).isChecked();
             if (checked == true){
                 Hours hour = new Hours (list[i].toString(), starttime.toString(), endtime.toString() );
                 userRef.child(MainActivity.getUser().getUsername()).child("clinic").child(MainActivity.getUser().getClinic().getClincName()).child(list[i].toString()).setValue(hour);
 
             }
-        }
+        }*/
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
 
-
-
-
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
