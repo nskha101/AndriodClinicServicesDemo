27222b76b7393cfb952dafa8f42cd24a8f03f3bd
nskha101 <nskha101@uottawa.ca>
2019-11-19 22:25:48 -0500

    validate clinic2

--- app/src/main/java/com/example/seg2105/CreateClinic.java
+++ app/src/main/java/com/example/seg2105/CreateClinic.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreateClinic extends AppCompatActivity {
 
@@ -27,6 +31,8 @@ public class CreateClinic extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.createbutton);
     }
 
+    public static final Pattern VALID_CLINIC_REGEX = Pattern.compile("^[\\p{L} .'-]+$", Pattern.CASE_INSENSITIVE);
+
     public void onClick(View view){
         //hello
         /*Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
@@ -41,5 +47,20 @@ public class CreateClinic extends AppCompatActivity {
 
         userRef.child(MainActivity.getUser().getUsername()).child("clinic").child(clinicName).setValue(new Clinic (clinicName));
 
+
+            if(!validateClinc(clinicName)){
+        clinicNameInput.setText("");
+        Toast.makeText(CreateClinic.this, "Invalid Clinic name",
+                Toast.LENGTH_LONG).show();
+    }
+        System.out.prinln(MainActivity.getUser().getUsername());
+
+        userRef.child(MainActivity.getUser().getUsername()).child("clinic").child(clinicName).setValue(new Clinic (clinicName));
+
+}
+
+    public static boolean validateClinc(String clinicname) {
+        Matcher matcher = VALID_CLINIC_REGEX.matcher(clinicname);
+        return matcher.find();
     }
 }
