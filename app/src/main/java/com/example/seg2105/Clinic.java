903664f14ec7dd0a8c210a40b7b59a151164f5c2
Lincoln <llayl068@uottawa.ca>
2019-11-19 20:49:04 -0500

    commit addclinic4

--- app/src/main/java/com/example/seg2105/Clinic.java
+++ app/src/main/java/com/example/seg2105/Clinic.java
@@ -1,50 +1,26 @@
 package com.example.seg2105;
 
-import java.util.Arrays;
-
 public class Clinic {
     private String clinicName;
-    private Service[] services;
-    private HoursofOperation[] hours;
-    int tailservice= -1;
-    int tailhours=-1;
-
 
 
     public Clinic (){
     }
 
     public Clinic (String clinicName){
-        this.clinicName=clinicName;
-        this.services= new Service[10];
-        this.hours= new HoursofOperation[7];
+       this.clinicName=clinicName;
     }
 
     public String getClincName(){
         return clinicName;
     }
 
-    public Service[] getServices(){
-        return services;
-    }
-
-    public HoursofOperation[] getHours() { return hours;}
-
-    public void addService(Service service ){
-        tailservice++;
-        services[tailservice]=service;
-    }
-
-    public void addHours( HoursofOperation hour){
-        tailhours++;
-        hours[tailhours]=hour;
-    }
-
     public void print(){
-        System.out.println("Clinic Name : "+clinicName+ " Services = "+ Arrays.toString(services) + " Hours = " + Arrays.toString(hours));
+        System.out.println("Clinic Name: "+clinicName);
     }
 
     public  String toString(){
-        return "Clinic Name = "+clinicName+ " Services = "+ Arrays.toString(services) + " Hours = " + Arrays.toString(hours);
+        return "Clinic Name: "+ clinicName;
     }
+
 }
