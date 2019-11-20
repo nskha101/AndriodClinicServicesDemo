e4bb4b45aff1283c59e423357f9d266f5f2b8c47
dstew056 <55411928+dstew056@users.noreply.github.com>
2019-11-19 22:25:23 -0500

    Added files for employee info

---
+++ app/src/main/java/com/example/seg2105/ChangeEmployeeInfo.java
@@ -0,0 +1,16 @@
+package com.example.seg2105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChangeEmployeeInfo extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_info_screen);
+    }
+}
