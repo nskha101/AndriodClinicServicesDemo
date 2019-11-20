e94a1e9fe34ec84674c3e7d724bde95229252fec
Bensmrz <55411938+Bensmrz@users.noreply.github.com>
2019-11-19 22:30:48 -0500

    EmployeeEditHours, Hours

--- app/src/main/java/com/example/seg2105/EmployeeHours.java
+++ app/src/main/java/com/example/seg2105/EmployeeHours.java
@@ -5,8 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class EmployeeHours extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference servicesRef = database.getReference();
+
+    final DatabaseReference userRef = database.getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +33,5 @@ public class EmployeeHours extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), EmployeeEditHours.class);
         startActivity(intent);
     }
+
 }
