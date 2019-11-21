37cd610bad4fa75c900b85a47cbb7f99b1536916
Alex Degrace <adegr091@uottawa.ca>
2019-11-21 10:48:30 -0500

    Work on AddClinicHours

--- app/src/main/java/com/example/seg2105/DeleteEmployeeService.java
+++ app/src/main/java/com/example/seg2105/DeleteEmployeeService.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,10 +20,18 @@ import java.util.ArrayList;
 public class DeleteEmployeeService extends AppCompatActivity {
 
     final ArrayList<Service> services = new ArrayList<>();
+    final ArrayList<Hours> hours = new ArrayList<>();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     private ArrayList<TextView> textViews = new ArrayList<>();
     final DatabaseReference userRef = database.getReference("users");
     Boolean alreadySeenService = false;
+
+    private TextView monday = findViewById(R.id.mondayLabel);
+    private TextView tuseday = findViewById(R.id.tuesdayLabel);
+    private TextView wednesday = findViewById(R.id.wednesdayLabel);
+    private TextView thursday = findViewById(R.id.thursdayLabel);
+    private TextView friday = findViewById(R.id.fridayLabel);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,8 @@ public class DeleteEmployeeService extends AppCompatActivity {
         setContentView(R.layout.activity_delete_employee_service);
 
         getAllServices();
+        getUserHours();
+
 
     }
 
@@ -64,6 +73,44 @@ public class DeleteEmployeeService extends AppCompatActivity {
         });
     }
 
+    public void getUserHours(){
+        userRef.child("users").child(MainActivity.getUser().getUsername()).child("clinic").child("hours").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //fet all the child of User
+                for(int i =0; i<hours.size(); i++){
+                    hours.set(i,null);
+                }
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                for (DataSnapshot child: children) {
+
+                    Hours currentChild = child.getValue(Hours.class);
+                    switch (currentChild.getDay()){
+                        case "Monday": hours.set(0,currentChild);
+                        break;
+                        case "Tuseday": hours.set(1,currentChild);
+                        break;
+                        case "Wednesday": hours.set(2,currentChild);
+                        break;
+                        case "Thursday": hours.set(3,currentChild);
+                        break;
+                        case "Friday": hours.set(4,currentChild);
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void seeDayHours(){
+
+    }
 
     public void seeServices(){
         TextView newTextView;
